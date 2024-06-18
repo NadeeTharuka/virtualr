92f12d02e9c2ab4ef51dda32bea07560a8c266fe
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-06-18 21:15:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Navbar from './components/Navbar';
-import HeroSection from "./components/HeroSection";
-import FeatureSection from "./components/FeatureSection";
-import Workflow from "./components/Workflow";
-import Footer from "./components/Footer";
-import Pricing from "./components/Pricing";
-import Testimonials from "./components/Testimonials";
+import Navbar from "./components/Navbar";
+import HeroSection from "../components/HeroSection";
+import FeatureSection from "../components/FeatureSection";
+import Workflow from "../components/Workflow";
+import Footer from "../components/Footer";
+import Pricing from "../components/Pricing";
+import Testimonials from "../components/Testimonials";
 
 const App = () => {
   return (
