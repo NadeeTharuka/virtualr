56cf56a5714c5965148479cf9c61bd1967d364b2
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-06-18 18:55:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello World!
-      </h1>
-    </div>
+    <>
+      <Navbar />
+      
+    </>
   )
 }
 
